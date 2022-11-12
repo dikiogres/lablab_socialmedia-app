529dc5a9354dd683493baaedf629955a462f6a31
dikiogres <dikiogres@gmail.com>
2022-11-12 23:16:44 +0700

    add: utils asset

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import { GoogleLogin, GoogleLogout  } from 'react-google-login';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <Link>
+            <Image
+                className="cursor-pointer"
+                
+            />
+        </Link>
+    </div>
   )
 }
 
