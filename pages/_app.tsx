5ac690de84ba3fabe2b6f632a0a4bd217a4b5ddb
dikiogres <dikiogres@gmail.com>
2022-11-12 22:44:07 +0700

    feat: SSR prevention init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps)  => {
+  const [ isSSR, setIsSSR ] = useState(true);
+
+  useEffect(() =>{
+    setIsSSR(false);
+  }, []);
+
+  if(isSSR){
+    return null
+  }
+
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
