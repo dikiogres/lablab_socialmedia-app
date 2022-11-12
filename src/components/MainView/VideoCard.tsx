3664587d0e58e1463300f6abfa79f902b5cf54d8
dikiogres <dikiogres@gmail.com>
2022-11-13 02:10:27 +0700

    feat: showing profile

--- src/components/MainView/VideoCard.tsx
+++ src/components/MainView/VideoCard.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Video } from '../../../types';
 import { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import { BsPlay } from 'react-icons/bs';
 
 interface IProps {
     post: Video;
@@ -8,7 +14,39 @@ interface IProps {
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
   return (
-    <div>VideoCard</div>
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+        <div>
+            <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+                <div className="md:w-16 md:h-16 w-10 h-10">
+                    <Link href="/">
+                        <>
+                        <Image
+                            width={62}
+                            height={62}
+                            className=' rounded-full'
+                            src={post.postedBy?.image}
+                            alt='user-profile'
+                            layout='responsive'
+                            />
+                        </>
+                    </Link>
+                </div>
+                <div >
+                    <Link href="/">
+                        <div className="flex items-center gap-2">
+                            <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                                {post.postedBy.userName}{``}
+                                <GoVerified className="text-blue-400 text-md"/>
+                            </p>     
+                            <p className="capitalize fond-medium text-xs text-gray-500 hidden md:block">
+                                {post.postedBy.userName}
+                            </p>       
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
