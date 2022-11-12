2b7ad5efacb56f424d83ebddf7d4c361968a888f
dikiogres <dikiogres@gmail.com>
2022-11-12 22:47:05 +0700

    feat: structure init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,15 @@ const MyApp = ({ Component, pageProps }: AppProps)  => {
 
   return (
     <div>
-      <Component {...pageProps} />
+      Navbar
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          Sidebar
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </div>
   )
 }
