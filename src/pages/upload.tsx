20757f5e07fc27d8e15a0764d994246602f9547c
dikiogres <dikiogres@gmail.com>
2022-11-17 17:53:19 +0700

    feat:handle post function

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -15,6 +15,12 @@ const Upload = () => {
     const [isLoading, setIsLoading ]= useState(false);
     const [videoAsset, setVideoAsset] = useState<SanityAssetDocument | undefined>();
     const [wrongFileType, setWrongFileType] = useState(false);
+    const [caption, setCaption] = useState('');
+    const [category, setCategory] = useState(topics[0].name);
+    const [savingPost, setSavingPost] = useState(false);
+
+    const { userProfile }: { userProfile:any } = useAuthStore;
+
 
     const uploadVideo = async (e:any) => {
         const selectedFile = e.target.files[0];
@@ -35,6 +41,30 @@ const Upload = () => {
         }else{
             setIsLoading(false);
             setWrongFileType(true);
+        }
+    }
+
+    const handlePost = async () => {
+        if(caption && videoAsset?._id && category){
+            setSavingPost(true);
+
+            const document = {
+                _type: 'post',
+                caption,
+                video: {
+                    _type: 'file', 
+                    asset: {
+                        _type: 'reference',
+                        _ref: videoAsset?._id
+                    }
+                },
+                userId: userProfile?._id,
+                postedBy: {
+                    _type: 'postedBy',
+                    _ref: userProfile?._id
+                },
+                topic: category
+            }
         }
     }
 
@@ -107,13 +137,13 @@ const Upload = () => {
                         <label className="text-md font-medium">Caption</label>
                         <input 
                             type="text" 
-                            value=""
-                            onChange={()=>{}}
+                            value={caption}
+                            onChange={(e)=>setCaption(e.target.value)}
                             className="rounder outline-none text-md border-2 border-gray-200 p-2"
                         />
                         <label className="text-md font-medium">Choose a Category</label>
                         <select 
-                            onChange={()=>{}}
+                            onChange={(e)=>setCategory(e.target.value)}
                             className="outline-none border-2 border-gray-200 text-md capitalize lg:p-4 rounded cursor-pointer p-2 hover:bg-slate-300"
                         >
                             {topics.map((topic)=>(
@@ -135,7 +165,7 @@ const Upload = () => {
                                 Discard
                             </button>
                             <button
-                                onClick={()=>{}}
+                                onClick={()=>{handlePost}}
                                 type="button"
                                 className="bg-[#55E2FE] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none"
                             >
